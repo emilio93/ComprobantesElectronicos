c431c584e69f107d8e1ebe2122e77e6111a94902
Emilio Rojas <emilio93@gmail.com>
2020-12-17 08:38:36 -0600

    docs: add documentation to Referencia

--- src/TiposComplejos/Referencia/InformacionReferencia.ts
+++ src/TiposComplejos/Referencia/InformacionReferencia.ts
@@ -9,10 +9,10 @@
 // THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 
 // Las referencias se utilizarán para los siguientes casos:
-//  a)Nota de crédito que elimina un documento de referencia en forma completa.
-//  b)Nota de débito que elimina una nota de crédito en la referencia en forma completa.
-//  c)Notas de crédito o débito que corrigen montos de otro documento.
-//  d)  Factura electrónica por contingencia, sustituyendo un comprobante provisional.
+//  a) Nota de crédito que elimina un documento de referencia en forma completa.
+//  b) Nota de débito que elimina una nota de crédito en la referencia en forma completa.
+//  c) Notas de crédito o débito que corrigen montos de otro documento.
+//  d) Factura electrónica por contingencia, sustituyendo un comprobante provisional.
 //  e) Cualquier documento relacionado con la factura electrónica.
 //  Nota: Se puede incluir un máximo de 10 repeticiones de información de referencia
 
@@ -31,12 +31,33 @@ export class InformacionReferencia {
    *                Ver nota 10 y 7
    */
   TipoDoc: string;
+  // 10/ La codificación a utilizar en el campo “Tipo de documento de referencia” es la siguiente, sin embargo para efectos de impresión y visualización se debe mostrar la descripción del código:
+  // Tipo de Documento Referencia                                                   Código
+  // Factura electrónica                                                            01
+  // Nota de débito electrónica                                                     02
+  // Nota de crédito electrónica                                                    03
+  // Tiquete electrónico                                                            04
+  // Nota de despacho                                                               05
+  // Contrato                                                                       06
+  // Procedimiento                                                                  07
+  // Comprobante emitido en contingencia                                            08
+  // Devolución mercadería                                                          09
+  // Sustituye factura rechazada por el Ministerio de Hacienda                      10
+  // Sustituye factura rechazada por el Receptor del comprobante                    11
+  // Sustituye Factura de exportación                                               12
+  // *Facturación mes vencido                                                       13
+  // Comprobante aportado por contribuyente del Régimende TributaciónSimplificado   14
+  // Sustituye una Factura electrónica de Compra                                    15
+  // Otros                                                                          99
+  // Nota: * Dicho campo se puede utilizar en los comprobantes electrónicos únicamente cuando el servicio prestado vence el último día del mes, como por ejemplo servicios públicos, los cuales se facturan en los primeros días del mes, pero los ingresos pertenecen al mes anterior; En el campo “fecha de emisión del documento de referencia” del apartado de referencia de debe de indicar la fecha del periodo fiscal que correspondeel ingreso dela factura.
+  // 7/ Para efectos de visualización e impresión se mostrará su descripción.
+
 
   /**
    * Nombre:        Clave numérica del comprobante electrónico o consecutivo del documento de referencia.
    * Tipo:          String
    * Tamaño:        50
-   * Descripcion:   Este campo será de condición obligatoria, cuando se incluya  información  en  el  campo  “Tipo  de documento de referencia”, exceptocuando se utilice el código 13
+   * Descripcion:   Este campo será de condición obligatoria, cuando se incluya  información  en  el  campo  “Tipo  de documento de referencia”, excepto cuando se utilice el código 13
    */
   Numero: string;
 
@@ -53,15 +74,24 @@ export class InformacionReferencia {
    * Tipo:          String
    * Tamaño:        2
    * Descripcion:   Es un campo fijo de dos posiciones
-   *                Ver nota 9 y 7 Este campo será de condiciónobligatoria, cuando se incluya información en el campo “Tipo de documento de referencia”, exceptocuando se utilice el código 13
+   *                Ver nota 9 y 7 Este campo será de condición obligatoria, cuando se incluya información en el campo “Tipo de documento de referencia”, excepto cuando se utilice el código 13
    */
   Codigo: string;
+  // 9/ Los códigos de referencia a utilizar son:
+  // Descripción del campo                               Código
+  // Anula Documento de Referencia                       01
+  // Corrige monto                                       02
+  // Referencia a otro documento                         04
+  // Sustituye comprobante provisional por contingencia. 05
+  // Otros                                               99
+  // 7/ Para efectos de visualización e impresión se mostrará su descripción.
+
 
   /**
    * Nombre:        Razón de referencia
    * Tipo:          String
    * Tamaño:        180
-   * Descripcion:   Este campo será de condición obligatoria, cuando seincluya  información  en  el  campo  “Tipo  de documento de referencia” excepto cuando se utilice el código 13
+   * Descripcion:   Este campo será de condición obligatoria, cuando se incluya  información  en  el  campo  “Tipo  de documento de referencia” excepto cuando se utilice el código 13
    */
   Razon: string;
 }
